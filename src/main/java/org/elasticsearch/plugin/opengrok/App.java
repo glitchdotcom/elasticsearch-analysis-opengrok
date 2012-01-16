0b9734987171bbb4ee5977b20d700539296980eb
Hao Lian <me@haolian.org>
2012-01-16 17:50:43 -0500

    Maven's quickstart archetype gave me this, now it is yours.

---
+++ src/main/java/org/elasticsearch/plugin/opengrok/App.java
@@ -0,0 +1,13 @@
+package org.elasticsearch.plugin.opengrok;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
