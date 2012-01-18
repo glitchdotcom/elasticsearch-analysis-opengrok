bd0ec3820741145ef789497522aca5af5c8abb86
Hao Lian <me@haolian.org>
2012-01-18 14:02:59 -0500

    install-opengrok.py: this is how you get OpenGrok into your local maven repository.

---
+++ scripts/install-opengrok.py
@@ -0,0 +1,1 @@
+cmd = "mvn install:install-file -Dfile=C:\Users\hao\Desktop\opengrok-0.10\lib\opengrok.jar '-DgroupId=org.opensolaris.opengrok' -DartifactId=opengrok -Dversion=0.10 -Dpackaging=jar -e"
